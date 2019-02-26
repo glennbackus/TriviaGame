05ed037fa528a6e4baf1fe9dc557ac73515e3428
glennbackus <46578905+glennbackus@users.noreply.github.com>
2019-02-26 00:48:59 -0500

    try

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+questions: {
+    q1: 'Who did Lary David base Seinfeld after?'
+    q2: 'Who was George engaged to?'
+    q3: 'In the season finale what was the group charged with?'
+    q4: 'What was the name of the Chicken restraunt across the street from Jerrys appartment?'
+    q5: 'What profession does George like to pretend he is?'
+    q6: 'What professional team did George work for?'
+}
+
+options: {
+    q1: ['Jerry', 'Elaine', 'Kramer', 'George'],
+    q2: ['Michelle', 'Susan', 'Allison', 'Samantha'],
+    q3: ['Doing nothing', 'DUI', 'Good Samaritan Law', 'White Collar Crime'],
+    q4: ['Pollo', 'Kenny Rodgers', 'Viva Chicken', 'None of the above'],
+    q5: ['Lawyer', 'NYPD', "Inport/Export", 'Architect'],
+    q6: ['NY Mets', 'NY Knicks', 'NY Rangers', 'NY Yankees']
+}
+
+answers: {
+    q1: 'George',
+    q2: 'Susan',
+    q3: 'Good Samaritan Law',
+    q4: 'Kenny Rodgers',
+    q5: 'Architect',
+    q6: 'NY Yankees'
+}
