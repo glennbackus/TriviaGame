bd4ec3f51f0362c05e2039d08669bf0be0c21906
glennbackus <46578905+glennbackus@users.noreply.github.com>
2019-02-26 21:35:28 -0500

    questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,32 @@
-questions: {
-    q1: 'Who did Lary David base Seinfeld after?'
-    q2: 'Who was George engaged to?'
-    q3: 'In the season finale what was the group charged with?'
-    q4: 'What was the name of the Chicken restraunt across the street from Jerrys appartment?'
-    q5: 'What profession does George like to pretend he is?'
-    q6: 'What professional team did George work for?'
-}
-
-options: {
-    q1: ['Jerry', 'Elaine', 'Kramer', 'George'],
-    q2: ['Michelle', 'Susan', 'Allison', 'Samantha'],
-    q3: ['Doing nothing', 'DUI', 'Good Samaritan Law', 'White Collar Crime'],
-    q4: ['Pollo', 'Kenny Rodgers', 'Viva Chicken', 'None of the above'],
-    q5: ['Lawyer', 'NYPD', "Inport/Export", 'Architect'],
-    q6: ['NY Mets', 'NY Knicks', 'NY Rangers', 'NY Yankees']
-}
-
-answers: {
-    q1: 'George',
-    q2: 'Susan',
-    q3: 'Good Samaritan Law',
-    q4: 'Kenny Rodgers',
-    q5: 'Architect',
-    q6: 'NY Yankees'
-}
+(function() {
+    var question = [{ 
+        question: "Who did Lary David base Seinfeld after?",
+        choices: [Jerry, Elaine, Kramer, George],
+        correctAnswer: George
+    },
+    {
+        question: "Who was George engaged to?",
+        choices: [Michelle, Susan, Allison, Samantha],
+        correctAnswer: Susan
+    }
+    {
+        question: "In the season finale what was the group charged with"?,
+        choices: [Doing nothing, DUI, Good Samaritan Law, White Collar Crime],
+        correctAnswer: Good Samaratin Law
+    }
+    {
+        question: "What was the name of the Chicken restraunt across the street from Jerrys appartment?",
+        choices: [Pollo, Kenny Rodgers, Viva Chicken, None of the above],
+        correctAnswer: Kenny Rodgers
+    }
+    {
+        question: "What profession does George like to pretend he is?",
+        choices: [Lawyer, NYPD, Inport/Export, Architect],
+        correctAnswer: Architect
+    }
+    {
+        question: "What professional team did George work for?",
+        choices: [NY Mets, NY Knicks, NY Rangers, NY Yankees],
+        correctAnswer: NY Yankees
+    }
+    }]
